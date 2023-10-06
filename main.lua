c7f0efbfc25c5db75984488b59a3d97485e5a0f9
Luka Krmpotić <luka.krmpotic@gmail.com>
2023-10-06 12:46:40 +0200

    prevent movement off screen

--- main.lua
+++ main.lua
@@ -11,12 +11,17 @@ function love.load()
 			dino[i][j] = love.graphics.newImage("pics/dino-" .. i .. "-" .. j .. ".png")
 		end
 	end
+	dino_w = 32
+	dino_h = 32
 
 	dir = DOWN
 	still = true
 	dino_loop = 1.0
 	v = 150
 	x, y = 100, 100
+
+	win_w = love.graphics.getWidth()
+	win_h = love.graphics.getHeight()
 
 	love.graphics.setBackgroundColor(204/255, 204/255, 204/255, 1)
 end
@@ -32,10 +37,11 @@ function love.draw()
 		t_ = 4 * (t_ % dino_loop)
 		i = math.floor(t_)
 	end
-	love.graphics.draw(dino[dir][i], x, y, 0, 1, 1, 16, 16)
+	love.graphics.draw(dino[dir][i], x, y, 0, 1, 1, dino_w/2, dino_h/2)
 end
 
 function love.update(dt)
+
 	local prev = still
 
 	still = false
@@ -54,6 +60,7 @@ function love.update(dt)
 	else
 		still = true
 	end
+	dinofixpos()
 
 	if still == false and prev ~= still then
 		t = love.timer.getTime()
@@ -63,5 +70,20 @@ end
 function love.keypressed(key, scancode, isrepeat)
 	if key == 'escape' or key == 'q' then
 		love.event.quit()
+	end
+end
+
+function dinofixpos()
+	if x < dino_w/2 then
+		x = dino_w/2
+	end
+	if x > win_w - dino_w/2 then
+		x = win_w - dino_w/2
+	end
+	if y < dino_h/2 then
+		y = dino_h/2
+	end
+	if y > win_h - dino_h/2 then
+		y = win_h - dino_h/2
 	end
 end
